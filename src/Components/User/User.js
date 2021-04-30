b18a512fd6543141e95964d300258338acdd4ac2
Kavya-Gowtham <kavyanagaraj20596@gmail.com>
2021-04-30 23:14:34 +0530

    Added User Page

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "./User.css";
 
 function User() {
+  function ChangePassword(e) {
+    console.log(e.target.name);
+    console.log(e.target);
+  }
   return (
-    <div>
-      <h1>THIS IS A USER PROFILE PAGE</h1>
+    <div className="user-profile">
+      <form className="user-form">
+        <label>
+          Username :
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Password :
+          <input type="Password" name="password" />
+        </label>
+      </form>
+      <button onClick={ChangePassword}>Change Password</button>
+      <button>Logout</button>
     </div>
   );
 }
